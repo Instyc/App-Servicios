fc8e325d66cf34f546e4e5d8a7de7c36ef7733b8
Ruggia <angelruggia@gmail.com>
2020-08-25 00:33:07 -0300

    Componente fila de publicacion

--- src/componentes/Categoria/Categoria.js
+++ src/componentes/Categoria/Categoria.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Filtro from './Filtro.js';
-
+import FilaPublicacion from './FilaPublicacion.js';
 export default function Categoria() {
     return (
         <div>
             <Filtro/>
+            <FilaPublicacion/>
+            <FilaPublicacion/>
+            <FilaPublicacion/>
         </div>
     )
 }
