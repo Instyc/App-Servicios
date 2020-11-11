e9f9c5d68c52da263b0549fa35ad0120d97749ef
Ferran <ferransolischorvat@gmail.com>
2020-11-11 18:45:49 -0300

    Updates

---
+++ precache-manifest.3e619263bc0e49d658047ce1ed6f8614.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e18f647f9ae57d9d0eb9dc1b56cdb96",
+    "url": "/App-Servicios/index.html"
+  },
+  {
+    "revision": "10f5e122ad3d72602c49",
+    "url": "/App-Servicios/static/css/2.be5d8eb4.chunk.css"
+  },
+  {
+    "revision": "a90b6034edb1181d08cb",
+    "url": "/App-Servicios/static/css/main.89838564.chunk.css"
+  },
+  {
+    "revision": "10f5e122ad3d72602c49",
+    "url": "/App-Servicios/static/js/2.194594bb.chunk.js"
+  },
+  {
+    "revision": "cee881da2c9c757478cb6c10f8550422",
+    "url": "/App-Servicios/static/js/2.194594bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a90b6034edb1181d08cb",
+    "url": "/App-Servicios/static/js/main.616e7935.chunk.js"
+  },
+  {
+    "revision": "aaa179ee8bbcc00d0931",
+    "url": "/App-Servicios/static/js/runtime-main.aae59b86.js"
+  },
+  {
+    "revision": "f4e4e06bd47045ce741425d0c1997949",
+    "url": "/App-Servicios/static/media/Miniatura.f4e4e06b.png"
+  }
+]);
